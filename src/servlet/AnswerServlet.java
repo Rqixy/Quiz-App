adfd1994ec712909d1baa5597d271f3f84e6e3dd
Rqixy <yuukisato1013@gmail.com>
2022-12-25 19:15:04 +0900

    Json形式でデータの受け取りや送信する方法を追加

--- src/servlet/AnswerServlet.java
+++ src/servlet/AnswerServlet.java
@@ -1,10 +1,10 @@
 package servlet;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.io.PrintWriter;
+import java.net.URLDecoder;
 
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,8 +12,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import model.AnswersBean;
-import model.QuizInfoBean;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import model.AnswerCheck;
 
 @WebServlet("/AnswerServlet")
 public class AnswerServlet extends HttpServlet {
@@ -21,27 +22,42 @@ public class AnswerServlet extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		try {
+			// 回答を取得する
+			
+			//. JSON テキストを全部取り出す
+		    BufferedReader br = new BufferedReader( request.getReader() );
+		    String jsonText = br.readLine();
+		    jsonText = URLDecoder.decode(jsonText, "UTF-8");
+		    
+		    ObjectMapper mapper = new ObjectMapper(); 
+		    
+		    AnswerCheck answerCheckObj = mapper.readValue(jsonText, AnswerCheck.class);
+		    
+		    String selectedAnswer = answerCheckObj.getSelectedAnswer(); 
+//		    System.out.println(selectedAnswer);
+		    
+			// quiz.jspにanswerCheckのデータをjson形式で送信する
+			
 			/* セッションスコープの準備 */
 			HttpSession session = request.getSession();
 
 			/* 問題リストと回答リストを取得 */
-			ArrayList<QuizInfoBean> quizList = (ArrayList<QuizInfoBean>)session.getAttribute("quizList");
-			ArrayList<AnswersBean> answerList = (ArrayList<AnswersBean>)session.getAttribute("answerList");
-
-			/* 問題番号を取得 */
-			int quizNumber = (int)session.getAttribute("quizNumber");
-
-			/* 問題リストと回答リストから出題された問題番号の要素を削除する */
-			quizList.remove(quizNumber);
-			answerList.remove(quizNumber);
-
-			// セッションスコープへオブジェクト(quizList)を保存
-			session.setAttribute("quizList", quizList);
+//			ArrayList<QuizInfoBean> quizList = (ArrayList<QuizInfoBean>)session.getAttribute("quizList");
+//			ArrayList<AnswersBean> answerList = (ArrayList<AnswersBean>)session.getAttribute("answerList");
+//
+//			/* 問題番号を取得 */
+//			int quizNumber = (int)session.getAttribute("quizNumber");
+//
+//			/* 問題リストと回答リストから出題された問題番号の要素を削除する */
+//			quizList.remove(quizNumber);
+//			answerList.remove(quizNumber);
+//
+//			// セッションスコープへオブジェクト(quizList)を保存
+//			session.setAttribute("quizList", quizList);
 
 			// 問題の答えを取得
 			String quizAnswer = (String)session.getAttribute("quizAnswer");
 			// ユーザーが選択した答えを取得
-			String selectedAnswer = request.getParameter("answer");
 			// 正解数のセッションを取得
 			int answerCount = (int)session.getAttribute("answerCount");
 
@@ -54,17 +70,26 @@ public class AnswerServlet extends HttpServlet {
 
 				// 正答数を+1してセッションに再保存する
 				answerCount++;
+				answerCheckObj.setCheckAnswer(true);
 				session.setAttribute("answerCount", answerCount);
 			}
 
+			String answerCheckJson = mapper.writeValueAsString(answerCheckObj);
+			response.setContentType("application/json;charset=UTF-8");
+			PrintWriter pw = response.getWriter();
+			pw.print(answerCheckJson);
+			pw.close();
+			
 			// セッションにtrueかfalseを保存する
 			session.setAttribute("answerCheck", answerCheck);
 
+			
+			
 			// 転送処理(フォワード)
-			// 問題画面へ表示
-			ServletContext s = request.getServletContext();
-			RequestDispatcher rd = s.getRequestDispatcher("/WEB-INF/view/answer.jsp");
-			rd.forward(request, response);
+//			// 問題画面へ表示
+//			ServletContext s = request.getServletContext();
+//			RequestDispatcher rd = s.getRequestDispatcher("/WEB-INF/view/quiz.jsp");
+//			rd.forward(request, response);
 		} catch(Exception e) {
 			// 例外処理
 			e.getStackTrace();
