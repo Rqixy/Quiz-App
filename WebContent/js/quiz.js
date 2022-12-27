1ae5360eabaac02b67e0cf0a3c03afb7b8b64044
Rqixy <yuukisato1013@gmail.com>
2022-12-27 22:02:43 +0900

    いくつかの処理を関数化

--- WebContent/js/quiz.js
+++ WebContent/js/quiz.js
@@ -1,8 +1,37 @@
 /**
- *
+ * 回答ボタンがクリックされた時に答えを表示し、次のページに遷移する処理
  */
 
- async function postData(url = '', data = {}) {
+const answerBtn = document.querySelectorAll(`button[type='submit'][name='answer']`);
+for (let i = 0; i < answerBtn.length; i++) {
+	answerBtn[i].addEventListener('click', () => {
+		const requestUrl = 'http://localhost:8080/QuizApp/AnswerServlet';
+		postData(requestUrl, { selectedAnswer: answerBtn[i].value })
+		.then((data) => {
+			// 受け取った結果にtrueがあったら、攻撃の画像を表示する
+			if (data['checkedAnswer']) {
+				const question2 = document.querySelector("#question-2");
+				createImageElement("./img/correct.png", 200, question2);
+			}
+	
+			// 結果が帰ってきたら、回答ボタンの上に○と×の画像を表示する
+			createImageElement("./img/mark_maru.png", 100, answerBtn[0]);
+			createImageElement("./img/mark_batsu.png", 100, answerBtn[1]);
+			createImageElement("./img/mark_batsu.png", 100, answerBtn[2]);
+			createImageElement("./img/mark_batsu.png", 100, answerBtn[3]);
+			
+			// 連射防止
+			disabledButton(answerBtn);
+			// 次も問題へ
+			nextQuestion(data['finished']);
+		}).catch((error) => {
+			console.log('Fetch API Error : ', error);
+		});
+	}, false);
+}
+
+// 非同期通信でサーブレットに送る設定
+async function postData(url = '', data = {}) {
 	const response = await fetch(url, {
 		method: 'POST',
 	    mode: 'cors',
@@ -19,45 +48,33 @@
 	return response.json();
 }
 
-const answerBtn = document.querySelectorAll(`button[type='submit'][name='answer']`);
-for (let i = 0; i < answerBtn.length; i++) {
-	answerBtn[i].addEventListener('click', function() {
-		const requestUrl = 'http://localhost:8080/QuizApp/AnswerServlet';
-		postData(requestUrl, { selectedAnswer: answerBtn[i].value })
-			.then((data) => {
-				// 受け取った結果にtrueがあったら、攻撃の画像を表示する
-				if (data['checkAnswer']) {
-					const correctImage = document.createElement('img');
-					correctImage.src = "./img/correct.png";
-					const question2 = document.querySelector("#question-2");
-					question2.appendChild(correctImage);
-				}
+// imgエレメントを作成する処理
+const createImageElement = (imageUrl = '', width = 0, element = null) => {
+	const imageElement = document.createElement('img');
+	imageElement.src = imageUrl;
+	imageElement.width = width;
+	element.appendChild(imageElement);
+}
 
-				// 結果が帰ってきたら、回答ボタンの上に○と×の画像を表示する
-				const markMaru = document.createElement('img');
-				markMaru.src = "./img/mark_maru.png";
-				answerBtn[0].appendChild(markMaru);
-				const markBatsu1 = document.createElement('img');
-				markBatsu1.src = "./img/mark_batsu.png";
-				answerBtn[1].appendChild(markBatsu1);
-				const markBatsu2 = document.createElement('img');
-				markBatsu2.src = "./img/mark_batsu.png";
-				answerBtn[2].appendChild(markBatsu2);
-				const markBatsu3 = document.createElement('img');
-				markBatsu3.src = "./img/mark_batsu.png";
-				answerBtn[3].appendChild(markBatsu3);
+// ボタンを1度しか押させない処理
+const disabledButton = (buttons = null) => {
+	for (let i = 0; i < buttons.length; i++) {
+		buttons[i].disabled = true;
+	}
+}
 
-				setTimeout(function() {
-					const form = document.createElement('form');
-					form.method = 'post';
-					if (data['finished']) {
-						form.action = 'http://localhost:8080/QuizApp/ResultServlet';
-					} else {
-						form.action = 'http://localhost:8080/QuizApp/QuizServlet';
-					}
-					document.body.appendChild(form)
-					form.submit();
-				}, 3000);
-			});
-	}, false);
+// 次の問題に遷移する処理
+const nextQuestion = (finishedQuiz = null) => {
+	setTimeout(() => {
+		const form = document.createElement('form');
+		form.method = 'post';
+		form.action = 'http://localhost:8080/QuizApp/';
+		if (finishedQuiz) {
+			form.action += 'ResultServlet';
+		} else {
+			form.action += 'QuizServlet';
+		}
+		document.body.appendChild(form);
+		form.submit();
+	}, 1000);
 }
