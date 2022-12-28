822474f5a7329134f256c91f06b4a1368b090462
Rqixy <yuukisato1013@gmail.com>
2022-12-28 16:18:19 +0900

    処理ごとに関数に分けたのとコメント整理

--- src/servlet/ResultServlet.java
+++ src/servlet/ResultServlet.java
@@ -28,21 +28,43 @@ public class ResultServlet extends HttpServlet {
 			request.setCharacterEncoding("UTF-8");
 			/* セッションスコープの準備 */
 			HttpSession session = request.getSession();
+			// 目標番号の取得
+			String goalNumber = (String)session.getAttribute("goalNumber");
+			
+			/* クリア状況の更新処理 */
+			updateClearStatus(session, goalNumber);
+	        /* セッションに目標番号のユニセフサイトのURLを保存 */
+	        session.setAttribute("goalUrl", "https://www.unicef.or.jp/kodomo/sdgs/17goals/" + geUnicefUrl(goalNumber));
 
-			/* DBに接続する */
+			// 結果画面へ表示
+			ServletContext s = request.getServletContext();
+			RequestDispatcher rd = s.getRequestDispatcher("/WEB-INF/view/result.jsp");
+			rd.forward(request, response);
+		} catch (ServletException e) {
+			System.out.println("ServletException : " + e.getMessage());
+		} catch (IOException e) {
+			System.out.println("IOException : " + e.getMessage());
+		} catch (Exception e) {
+			System.out.println("Exception : " + e.getMessage());
+		}
+	}
+	
+	/* クリア状況を更新する処理 */
+	private void updateClearStatus(HttpSession session, String goalNumber) {
+		try {
+			/* DBに接続 */
 			Db db = new Db();
 			Connection con = db.DbConnection();
 			// 初期化
 			String sql = "";
 			PreparedStatement ps = null;
 			ResultSet rs = null;
-			ResultSetMetaData md = null;
-
-			/* セッションスコープに保存しておいたユーザーIDと目標番号を取得する */
+			ResultSetMetaData rsmd = null;
+	
+			/* セッションに保存しておいたユーザーIDと目標番号を取得 */
 			int userId = (int)session.getAttribute("userId");
-	        String goalNumber = (String)session.getAttribute("goalNumber");
-
-	        /* ハッシュ配列に目標番号とclear_statusテーブルのカラム名を格納する */
+	
+	        /* ハッシュ配列に目標番号とclear_statusテーブルのカラム名を格納 */
 			// SQL文の発行
 			sql = "SELECT * FROM clear_status WHERE user_id = ?";
 			// プリペアードステートメント作成
@@ -51,95 +73,63 @@ public class ResultServlet extends HttpServlet {
 			ps.setInt(1, userId);
 			// 実行
 			rs = ps.executeQuery();
-			md = ps.getMetaData();
-
+			rsmd = ps.getMetaData();
+	
 			/* ハッシュ配列に目標番号とclear_statusテーブルのカラム名を格納する */
 			HashMap<String, String> clearStatusColumns = new HashMap<String, String>();
-			// clear_statusテーブルのカラム数を取得する
-			int columnCount = md.getColumnCount();
-			// ハッシュ配列に目標番号とそのカラム名を格納する
-			for (int i = 2; i <= columnCount; i++) {
+			// clear_statusテーブルのカラム数を取得
+			int clearStatusColumnCount = rsmd.getColumnCount();
+			// ハッシュ配列に目標番号とそのカラム名を格納
+			for (int i = 2; i <= clearStatusColumnCount; i++) {
 				//clear_statusテーブルの目標番号
 				String clearStatusGoalNumber = Integer.valueOf(i-1).toString();
 				// ハッシュ配列に格納
-				clearStatusColumns.put(clearStatusGoalNumber, md.getColumnName(i));
+				clearStatusColumns.put(clearStatusGoalNumber, rsmd.getColumnName(i));
 			}
-			/* 今回挑戦したクイズのカラム名を取得する */
-			String clearStatusColumn = clearStatusColumns.get(goalNumber);
-
+			/* ユーザーが選択した目標番号のクイズのカラム名を取得 */
+			String selectedClearStatusColumnName = clearStatusColumns.get(goalNumber);
+	
 			/* 更新前のクリア状況を取得する */
 			// SQL文の発行
-			sql = "SELECT " + clearStatusColumn + " FROM clear_status WHERE user_id = ?";
+			sql = "SELECT " + selectedClearStatusColumnName + " FROM clear_status WHERE user_id = ?";
 			// プリペアードステートメント作成
 			ps = con.prepareStatement(sql);
 			// 値をセットする
 			ps.setInt(1, userId);
 			// 実行
 			rs = ps.executeQuery();
-
+	
 			// DB内のクリア状況の取得
 			int clearStatusInDb = 0;
 			if (rs.next()) {
 				clearStatusInDb = rs.getInt(clearStatusColumns.get(goalNumber));
 			}
-
-			/* 正答数を確認して、正答数に応じたクリア状況を保存する */
+	
+			/* 正答数を確認して、正答数に応じたクリア状況を保存 */
 			float answerCount = (int)session.getAttribute("answerCount");
 			float maxQuizCount = (int)session.getAttribute("maxQuizCount");
-
+	
 			// 正答率を求める
 			float correctAnswerRate = answerCount / maxQuizCount;
 			if (clearStatusInDb < 2 && correctAnswerRate == 1) {
 				// 全問正解したことがなく、初めて全問正解なら数字を2に更新する
-				updateClearStatus(clearStatusColumn, 2, userId);
+				executeUpdateClearStatus(selectedClearStatusColumnName, 2, userId);
 			} else if (clearStatusInDb == 0 && correctAnswerRate >= 0.6f) {
 				// もしまだ6割未満(数字が0)で、今回正答率が6割超えたら1に更新する
-				updateClearStatus(clearStatusColumn, 1, userId);
+				executeUpdateClearStatus(selectedClearStatusColumnName, 1, userId);
 			}
-
+	
 			/* DBへの接続開放(クローズ処理) */
 			con.close();
 			ps.close();
 			rs.close();
-
-			/* ハッシュ配列で目標番号とSDGsの17の目標のユニセフサイトのURLを格納する */
-			// ハッシュ配列の初期化
-			HashMap<String, String> goals = new HashMap<String, String>();
-
-			// ハッシュ配列に目標番号と17の目標のURLを格納する
-			goals.put("1", "1-poverty");
-	        goals.put("2", "2-hunger");
-	        goals.put("3", "3-health");
-	        goals.put("4", "4-education");
-	        goals.put("5", "5-gender");
-	        goals.put("6", "6-water");
-	        goals.put("7", "7-energy");
-	        goals.put("8", "8-economic_growth");
-	        goals.put("9", "9-industry");
-	        goals.put("10", "10-inequalities");
-	        goals.put("11", "11-cities");
-	        goals.put("12", "12-responsible");
-	        goals.put("13", "13-climate_action");
-	        goals.put("14", "14-sea");
-	        goals.put("15", "15-land");
-	        goals.put("16", "16-peace");
-	        goals.put("17", "17-partnerships");
-
-	        /* セッションスコープに目標番号のユニセフサイトのURLを保存する */
-	        session.setAttribute("goalUrl", "https://www.unicef.or.jp/kodomo/sdgs/17goals/" + goals.get(goalNumber));
-
-			// 転送処理(フォワード)
-			// 結果画面へ表示
-			ServletContext s = request.getServletContext();
-			RequestDispatcher rd = s.getRequestDispatcher("/WEB-INF/view/result.jsp");
-			rd.forward(request, response);
-		} catch(Exception e) {
-			// 例外処理
-			e.getStackTrace();
+		} catch (SQLException e) {
+			System.out.println("SQLException : " + e.getMessage());
 		}
 	}
 	
-	private void updateClearStatus(String clearStatusColumn, int updateStatus, int userId) {
+	/* DBにクリア状況を更新する処理 */
+	private void executeUpdateClearStatus(String clearStatusColumn, int updateStatus, int userId) {
 		try {
 			/* DBに接続する */
 			Db db = new Db();
@@ -160,8 +150,36 @@ public class ResultServlet extends HttpServlet {
 			con.close();
 			ps.close();
 		} catch(SQLException e) {
-			System.out.println(e.getMessage());
+			System.out.println("SQLException : " + e.getMessage());
 		}
 		
+	}
+	
+	/* 目標番号からユニセフのURLを取得する処理 */
+	private String geUnicefUrl(String goalNumber) {
+		/* ハッシュ配列で目標番号とSDGsの17の目標のユニセフサイトのURLを格納する */
+		// ハッシュ配列の初期化
+		HashMap<String, String> goals = new HashMap<String, String>();
+
+		// ハッシュ配列に目標番号と17の目標のURLを格納する
+		goals.put("1", "1-poverty");
+        goals.put("2", "2-hunger");
+        goals.put("3", "3-health");
+        goals.put("4", "4-education");
+        goals.put("5", "5-gender");
+        goals.put("6", "6-water");
+        goals.put("7", "7-energy");
+        goals.put("8", "8-economic_growth");
+        goals.put("9", "9-industry");
+        goals.put("10", "10-inequalities");
+        goals.put("11", "11-cities");
+        goals.put("12", "12-responsible");
+        goals.put("13", "13-climate_action");
+        goals.put("14", "14-sea");
+        goals.put("15", "15-land");
+        goals.put("16", "16-peace");
+        goals.put("17", "17-partnerships");
+        
+        return goals.get(goalNumber);
 	}
 }
