0d9c75f797c5c80d3289208c9a3b9d86a8fdedb7
Rqixy <yuukisato1013@gmail.com>
2022-12-30 19:07:15 +0900

    DB操作をQuizQueryクラスに移動

--- src/servlet/QuizServlet.java
+++ src/servlet/QuizServlet.java
@@ -1,9 +1,6 @@
 package servlet;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Random;
@@ -17,7 +14,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import db.Db;
+import db.QuizQuery;
 import model.AnswersBean;
 import model.QuizInfoBean;
 
@@ -29,18 +26,19 @@ public class QuizServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.setCharacterEncoding("UTF-8");
-		/* リストからクイズ問題を取得 */
 		try {
-			/* POSTで「goalNumber」を取得する */
+			request.setCharacterEncoding("UTF-8");
+			// セッションスコープの準備
+			HttpSession session = request.getSession();
+			// POSTで「goalNumber」を取得
 			String goalNumber = request.getParameter("goalNumber");
 			
 			if (goalNumber != null) {
-				/* goalNumberのパラメータが存在していたら、送信された目標番号の問題を準備する */
-				prepareQuiz(request, goalNumber);
+				// goalNumberのパラメータが存在していたら、送信された目標番号の問題を準備する
+				prepareQuiz(session, goalNumber);
 			} else {
-				/* goalNumberのパラメータが存在していなかったら、２問目以降なので、その処理を行う*/
-				prepareNextQuiz(request);
+				// goalNumberのパラメータが存在していなかったら、２問目以降なので、その処理を行う
+				prepareNextQuiz(session);
 			}
 
 			// 問題画面へ表示
@@ -56,74 +54,21 @@ public class QuizServlet extends HttpServlet {
 		}
 	}
 	
-	/* 送信された目標番号の問題を準備する処理 */
-	private void prepareQuiz(HttpServletRequest request, String goalNumber) {
+	// 送信された目標番号の問題を準備する処理
+	private void prepareQuiz(HttpSession session, String goalNumber) {
 		try {
-			/* セッションスコープの準備 */
-			HttpSession session = request.getSession();
+			// DB内のクイズ情報と回答情報を操作するクラスの初期化
+			QuizQuery quizQuery = new QuizQuery();
 			
-			// DBの接続準備
-			Db db = new Db();
-			Connection con = db.DbConnection();
-			String sql = "";
-			PreparedStatement ps = null;
-			ResultSet rs = null;
-			
-			// クイズ問題一覧を取得するSQL文の発行
-			sql = "SELECT quiz_id, quiz, answer, commentary FROM quiz_info WHERE goal_number = ?";
-			ps = con.prepareStatement(sql);
-			// 値をセット
-			ps.setString(1, goalNumber);
-			// 実行
-			rs = ps.executeQuery();
-			
-			// クイズ情報をオブジェクトに格納する
-			ArrayList<QuizInfoBean> quizList = new ArrayList<>();
-			while(rs.next()) {
-				QuizInfoBean quizInfoObj = new QuizInfoBean();
-				// DBから取得したクイズ情報をQuizInfoBeanオブジェクトのクイズ情報へ代入
-				quizInfoObj.setQuiz_id(rs.getInt("quiz_id"));
-				quizInfoObj.setQuiz(rs.getString("quiz"));
-				quizInfoObj.setAnswer(rs.getString("answer"));
-				quizInfoObj.setCommentary(rs.getString("commentary"));
-				// QuizInfoBeanオブジェクトをクイズリスト(quizList)の要素へ追加
-				quizList.add(quizInfoObj);
-			}
-			
-			// 回答一覧を取得するSQL文の発行
-			sql = "SELECT a.answer_id, a.correct, a.incorrect_1, a.incorrect_2, a.incorrect_3 "
-			    + "FROM answers AS a JOIN quiz_info AS q ON q.quiz_id = answer_id "
-			    + "WHERE q.goal_number = ?";
-			ps = con.prepareStatement(sql);
-			// 値をセット
-			ps.setString(1, goalNumber);
-			// 実行
-			rs = ps.executeQuery();
-	
-			// 回答情報をオブジェクトに格納する
-			ArrayList<AnswersBean> answerList = new ArrayList<>();
-			while(rs.next()) {
-				AnswersBean answersObj = new AnswersBean();
-				// DBから取得したクイズ情報をAnswersBeanオブジェクトのクイズ情報へ代入
-				answersObj.setAnswers_id(rs.getInt("answer_id"));
-				answersObj.setCorrect(rs.getString("correct"));
-				answersObj.setIncorrect_1(rs.getString("incorrect_1"));
-				answersObj.setIncorrect_2(rs.getString("incorrect_2"));
-				answersObj.setIncorrect_3(rs.getString("incorrect_3"));
-				// AnswersBeanオブジェクトを回答リスト(answerList)の要素へ追加
-				answerList.add(answersObj);
-			}
-	
-			/* DBへの接続開放(クローズ処理) */
-			con.close();
-			ps.close();
-			rs.close();
+			// クイズ情報と回答情報をオブジェクトに格納
+			ArrayList<QuizInfoBean> quizList = quizQuery.getQuizList(goalNumber);
+			ArrayList<AnswersBean> answerList = quizQuery.getAnswerList(goalNumber);
 			
 			// 問題をランダムに表示する番号
 			Random randomNumber = new Random();
 			int quizNumber = randomNumber.nextInt(quizList.size());
 	
-			/* セッションスコープへ、クイズ情報と回答一覧を保存する */
+			// セッションへ使いまわす情報を保存
 			session.setMaxInactiveInterval(600);					// セッションの保存期間の設定(10分)
 			session.setAttribute("goalNumber", goalNumber);			// 目標番号
 			session.setAttribute("quizList", quizList);				// クイズ情報
@@ -137,11 +82,8 @@ public class QuizServlet extends HttpServlet {
 		}
 	}
 	
-	/* ２問目以降の問題準備処理 */
-	private void prepareNextQuiz(HttpServletRequest request) {
-		/* セッションスコープの準備 */
-		HttpSession session = request.getSession();
-		
+	// ２問目以降の問題準備処理
+	private void prepareNextQuiz(HttpSession session) {
 		/* 問題リストと回答リストを取得 */
 		ArrayList<QuizInfoBean> quizList = (ArrayList<QuizInfoBean>)session.getAttribute("quizList");
 		ArrayList<AnswersBean> answerList = (ArrayList<AnswersBean>)session.getAttribute("answerList");
