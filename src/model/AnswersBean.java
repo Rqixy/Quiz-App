c98538897376c36a2953ff73fd0a937f14fa055c
Rqixy <67447178+Rqixy@users.noreply.github.com>
2022-12-31 16:59:43 +0900

    コメント削除

--- src/model/AnswersBean.java
+++ src/model/AnswersBean.java
@@ -1,16 +1,13 @@
 package model;
 /*
- * 回答一覧を管理するクラス
+ * 回答一覧管理クラス
  */
 public class AnswersBean {
-
-	// フィールド
 	private String correct;		// 正解
 	private String incorrect1;	// 誤答1
 	private String incorrect2;	// 誤答2
 	private String incorrect3;	// 誤答3
 
-	// コンストラクタ
 	public AnswersBean(String correct, String incorrect1, String incorrect2, String incorrect3) {
 		this.correct = correct;
 		this.incorrect1 = incorrect1;
@@ -18,7 +15,6 @@ public class AnswersBean {
 		this.incorrect3 = incorrect3;
 	}
 
-	// アクセサメソッド
 	public String correct() {
 		return correct;
 	}
--- src/model/QuizInfoBean.java
+++ src/model/QuizInfoBean.java
@@ -3,18 +3,14 @@ package model;
  * クイズ情報管理クラス
  */
 public class QuizInfoBean {
-
-	// フィールド
 	private String quiz;		// 問題
 	private String answer;		// 回答
 
-	// コンストラクタ
 	public QuizInfoBean(String quiz, String answer) {
 		this.quiz = quiz;
 		this.answer = answer;
 	}
 
-	// アクセサメソッド
 	public String quiz() {
 		return quiz;
 	}
