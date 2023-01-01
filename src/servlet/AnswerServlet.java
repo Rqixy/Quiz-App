d74abde4714654011513cb7d7453f5cf7d9e22f4
Rqixy <67447178+Rqixy@users.noreply.github.com>
2023-01-01 11:33:45 +0900

    JSON関連の実装をJSONディレクトリに移動

--- src/servlet/AnswerServlet.java
+++ src/servlet/AnswerServlet.java
@@ -1,9 +1,7 @@
 package servlet;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.net.URLDecoder;
+import java.util.HashMap;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,9 +10,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.JsonNode;
 
-import model.AjaxAnswerInfo;
+import json.Json;
 
 @WebServlet("/AnswerServlet")
 public class AnswerServlet extends HttpServlet {
@@ -26,45 +24,37 @@ public class AnswerServlet extends HttpServlet {
 			// セッションスコープの準備
 			HttpSession session = request.getSession();
 			
-			// 非同期で送られてきたJSONファイルから、ユーザーが選択した答えを取得
-		    BufferedReader br = new BufferedReader(request.getReader());
-		    ObjectMapper ob = new ObjectMapper();
+			Json json = new Json();
+			JsonNode selectedAnswerJson = json.jsonByHttpServletRequest(request);
+		    // 選択した答えの取得
+		    String selectedAnswer = selectedAnswerJson.get("selectedAnswer").textValue();
 		    
-		    String selectedAnswerJsonText = br.readLine();
-		    selectedAnswerJsonText = URLDecoder.decode(selectedAnswerJsonText, "UTF-8");
-		    
-		    // JSONからオブジェクトに変換する
-		    AjaxAnswerInfo ajaxAnswerInfo = ob.readValue(selectedAnswerJsonText, AjaxAnswerInfo.class);
-			
-			// 現在の問題番号とクイズの最大問題数を取得する
-			int currentQuizCount = (int)session.getAttribute("currentQuizCount");
-			int maxQuizCount = (int)session.getAttribute("maxQuizCount");
-			// 出力した問題数が最大値に達したら、finishedをtrueに変更する
-			if (currentQuizCount >= maxQuizCount) {
-				ajaxAnswerInfo.setFinished(true);
-			}
+		    // 答えの判定と全問終了した判定のハッシュ配列
+		    HashMap<String, Boolean> checked = new HashMap<String, Boolean>();
+	    	checked.put("isCorrect", false);
+	    	checked.put("isFinished", false);
 
 			// 問題の答えと正解数を取得
 			String quizAnswer = (String)session.getAttribute("quizAnswer");
 			int answerCount = (int)session.getAttribute("answerCount");
-
 			// 問題の答えと選択した答えが一致しているか判定
-			// 回答を取得
-			String selectedAnswer = ajaxAnswerInfo.getSelectedAnswer();
 			if(quizAnswer.equals(selectedAnswer)) {
-				// checkedAnswerをtrueにする
-				ajaxAnswerInfo.setCheckedAnswer(true);
+				// isCorrectをtrueにする
+				checked.put("isCorrect", true);
 				// 正答数を+1してセッションに再保存
 				answerCount++;
 				session.setAttribute("answerCount", answerCount);
 			}
 			
-			// 回答結果のオブジェクトをJSON形式に書き直して、表に返す
-			String ajaxAnswerJson = ob.writeValueAsString(ajaxAnswerInfo);
-			response.setContentType("application/json;charset=UTF-8");
-			PrintWriter pw = response.getWriter();
-			pw.print(ajaxAnswerJson);
-			pw.close();
+			// 現在の問題番号とクイズの最大問題数を取得する
+			int currentQuizCount = (int)session.getAttribute("currentQuizCount");
+			int maxQuizCount = (int)session.getAttribute("maxQuizCount");
+			// 出力した問題数が最大値に達したら、isFinishedをtrueに変更する
+			if (currentQuizCount >= maxQuizCount) {
+				checked.put("isFinished", true);
+			}
+			
+			json.checkedMapToStrJsonAndResponse(checked, response);
 		} catch (IOException e) {
 			System.out.println("IOException : " + e.getMessage());
 		} catch (Exception e) {
