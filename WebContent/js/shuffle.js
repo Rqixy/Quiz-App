c5dc6dfa4ca426b8854c509fbdf5e42278cfdcc9
Rqixy <yuukisato1013@gmail.com>
2023-01-02 14:39:34 +0900

    要素の受け取り処理変更

--- WebContent/js/shuffle.js
+++ WebContent/js/shuffle.js
@@ -1,6 +1,16 @@
 /**
  * 回答ボタンの順番をランダムに表示させる
  */
+document.addEventListener('DOMContentLoaded', function() {
+	const answers = document.querySelectorAll("#answers");
+	const buttons = answers.querySelectorAll("button")
+	const randomButtons = shuffle(buttons);
+
+	answers.innerHTML = '';
+	randomButtons.forEach(button => {
+		answers.appendChild(button);
+	});
+});
 
 const shuffle = ([...arr]) => {
 	let m = arr.length;
@@ -11,14 +21,3 @@ const shuffle = ([...arr]) => {
 
 	return arr;
 }
-
-document.addEventListener('DOMContentLoaded', function() {
-	const answers = document.getElementById("answers");
-	const buttons = answers.getElementsByTagName("button")
-	const randomButtons = shuffle(buttons);
-
-	answers.innerHTML = '';
-	randomButtons.forEach(button => {
-		answers.appendChild(button);
-	});
-});
