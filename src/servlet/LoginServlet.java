18ea3d41b083f172f4e7ae3daec8d21b7c240161
Rqixy <yuukisato1013@gmail.com>
2023-01-02 14:34:43 +0900

    セッション管理確認用ログイン処理追加（後で削除する）

---
+++ src/servlet/LoginServlet.java
@@ -0,0 +1,27 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// Login画面へ表示
+		ServletContext s = request.getServletContext();
+		RequestDispatcher rd = s.getRequestDispatcher("/WEB-INF/view/login.jsp");
+		rd.forward(request, response);
+	}
+
+}
