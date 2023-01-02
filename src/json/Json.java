62042060f54403ed4b1e63837969545a7dbcc0bf
Rqixy <yuukisato1013@gmail.com>
2023-01-02 14:33:54 +0900

    コメント追加とフラグハッシュ配列変換処理追加

--- src/json/Json.java
+++ src/json/Json.java
@@ -3,52 +3,79 @@ package json;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.util.HashMap;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+/**
+ * JSON関連の操作をまとめたクラス
+ */
 public class Json {
+	private ObjectMapper ob = new ObjectMapper();
+	
 	/**
 	 * サーブレットのレスポンスから、渡ってきたJSONを取得する
 	 * @param request	リクエスト
 	 * @return json		JSON
 	 * @throws IOException
 	 */
-	public JsonNode jsonByHttpServletRequest(HttpServletRequest request) throws IOException {
+	public JsonNode jsonByServletRequest(HttpServletRequest request) throws JsonMappingException, JsonProcessingException, UnsupportedEncodingException, IOException {
 		JsonNode json = null;
 		try {
 		    BufferedReader br = new BufferedReader(request.getReader());
-		    ObjectMapper ob = new ObjectMapper();
 		    // String型のJSONファイル取得
 		    String jsonText = br.readLine();
 		    jsonText = URLDecoder.decode(jsonText, "UTF-8");
 		    
 		    json = ob.readTree(jsonText);
+		} catch (JsonMappingException e) {
+			System.out.println("JsonMappingException : " + e.getMessage());
+		} catch (JsonProcessingException e) {
+			System.out.println("JsonProcessingException : " + e.getMessage());
+		} catch (UnsupportedEncodingException e) {
+			System.out.println("UnsupportedEncodingException : " + e.getMessage());
 		} catch (IOException e) {
 			System.out.println("IOException : " + e.getMessage());
-		}
+		} 
 		return json;
 	}
 	
 	/**
-	 * checkedハッシュ配列をJSONに変換してレスポンスする
-	 * @param checked
-	 * @param response
+	 * フラグハッシュ配列を文字列型のJSONに変える処理
+	 * @param  flag		フラグ
+	 * @return strJsonFlag	文字列型のJSONフラグ
+	 * @throws JsonProcessingException
+	 */
+	public String flagMapToString(HashMap<String, Boolean> flagMap) throws JsonProcessingException {
+		String strJsonFlag = "";
+		try {
+			strJsonFlag = ob.writeValueAsString(flagMap);
+		} catch (JsonProcessingException e) {
+			System.out.println("JsonProcessingException : " + e.getMessage());
+		}
+		
+		return strJsonFlag;
+	}
+	
+	/**
+	 * 文字列型JSONをフロントにレスポンスする処理
+	 * @param  strJson	
+	 * @param  response
 	 * @throws IOException
 	 */
-	public void checkedMapToStrJsonAndResponse(HashMap<String, Boolean> checked, HttpServletResponse response) throws IOException {
+	public void ResponseStringJson(HttpServletResponse response, String strJson) throws IOException {
 		try {
-			ObjectMapper ob = new ObjectMapper();
-			// 回答結果のハッシュ配列をJSONの文字列に書き直して、表に返す
-			String strChecked = ob.writeValueAsString(checked);
 			response.setContentType("application/json;charset=UTF-8");
 			PrintWriter pw = response.getWriter();
-			pw.print(strChecked);
+			pw.print(strJson);
 			pw.close();
 		} catch (IOException e) {
 			System.out.println("IOException : " + e.getMessage());
