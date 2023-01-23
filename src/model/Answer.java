42287c20e77ae3103666e1b07a4912f5a1652c73
Rqixy <yuukisato1013@gmail.com>
2023-01-23 16:42:59 +0900

    Answerサーブレットの処理をmodelのAnswerクラスに移動

---
+++ src/model/Answer.java
@@ -0,0 +1,50 @@
+package model;
+
+import java.util.HashMap;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+
+import json.Json;
+
+public class Answer {
+	private static HashMap<String, Boolean> flag;
+	private final String quizAnswer;
+	private final String selectedAnswer;
+	
+	Answer(final String quizAnswer, final String selectedAnswer) {
+		Answer.flag = new HashMap<String, Boolean>();
+		this.quizAnswer = quizAnswer;
+		this.selectedAnswer = selectedAnswer;
+	}
+	
+	/**
+	 * 
+	 * @param quiz
+	 * @param selectedAnswerJson
+	 * @return Json.flagMapToString(flag)
+	 * @throws JsonProcessingException
+	 */
+	public static String correctAndFinishedFlagToText(final Quiz quiz, final JsonNode selectedAnswerJson) throws JsonProcessingException {
+	    // 問題の答えと選択した答えを取得
+	    Answer answer = new Answer(quiz.quizInfoBean().answer(), selectedAnswerJson.get("selectedAnswer").textValue());
+	    
+	    // 答えの判定と全問終了した判定のハッシュ配列
+	    flag.put("isCorrect", false);
+	    flag.put("isFinished", false);
+		// 問題の答えと選択した答えが一致しているか判定
+		if(answer.quizAnswer.equals(answer.selectedAnswer)) {
+			// isCorrectをtrueにする
+			flag.put("isCorrect", true);
+			// 正答数を+1
+			quiz.addAnswerCount();
+		}
+		
+		// 出力した問題数が最大値に達したら、isFinishedをtrueに変更する
+		if (quiz.currentQuizCount() >= quiz.maxQuizCount()) {
+			flag.put("isFinished", true);
+		}
+		
+		return Json.flagMapToString(flag);
+	}
+}
--- src/servlet/AnswerServlet.java
+++ src/servlet/AnswerServlet.java
@@ -2,7 +2,6 @@ package servlet;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
-import java.util.HashMap;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -16,9 +15,9 @@ import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
 
 import json.Json;
+import model.Answer;
 import model.Login;
 import model.Quiz;
-import model.QuizInfoBean;
 import model.ScreenTransition;
 
 /**
@@ -48,37 +47,12 @@ public class AnswerServlet extends HttpServlet {
 			// セッションスコープの準備
 			HttpSession session = request.getSession();
 			
-			Json json = new Json();
-			JsonNode selectedAnswerJson = json.jsonByServletRequest(request);
-		    // 選択した答えの取得
-		    String selectedAnswer = selectedAnswerJson.get("selectedAnswer").textValue();
+			Quiz quiz = (Quiz)session.getAttribute("quiz");
+			JsonNode selectedAnswerJson = Json.jsonByServletRequest(request);
 		    
-		    // 答えの判定と全問終了した判定のハッシュ配列
-		    HashMap<String, Boolean> flag = new HashMap<String, Boolean>();
-		    flag.put("isCorrect", false);
-		    flag.put("isFinished", false);
-
-			// 問題の答えと正解数を取得
-		    Quiz quiz = (Quiz)session.getAttribute("quiz");
-		    QuizInfoBean quizInfoBean = quiz.quizInfoBean();
-			String quizAnswer = quizInfoBean.answer();
-			
-			// 問題の答えと選択した答えが一致しているか判定
-			if(quizAnswer.equals(selectedAnswer)) {
-				// isCorrectをtrueにする
-				flag.put("isCorrect", true);
-				// 正答数を+1
-				quiz.addAnswerCount();
-			}
-			
-			// 出力した問題数が最大値に達したら、isFinishedをtrueに変更する
-			if (quiz.currentQuizCount() >= quiz.maxQuizCount()) {
-				flag.put("isFinished", true);
-			}
-			
 			// フロントに答えと全問終了かどうかを返す
-			String strFlag = json.flagMapToString(flag);
-			json.ResponseStringJson(response, strFlag);
+			String strFlag = Answer.correctAndFinishedFlagToText(quiz, selectedAnswerJson);
+			Json.ResponseStringJson(response, strFlag);
 		} catch (JsonMappingException e) {
 			System.out.println("JsonMappingException : " + e.getMessage());
 		} catch (JsonProcessingException e) {
