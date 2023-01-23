536034fe42bfcbec512a55408f042a9b4414c15f
Rqixy <yuukisato1013@gmail.com>
2023-01-23 11:03:40 +0900

    quizListを取得する処理追加し、フィールドを定数化、コンストラクタをprivateに変更

--- src/model/QuizInfoBean.java
+++ src/model/QuizInfoBean.java
@@ -1,16 +1,43 @@
 package model;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 /*
  * クイズ情報管理クラス
  */
 public class QuizInfoBean {
-	private String quiz;		// 問題
-	private String answer;		// 回答
-
-	public QuizInfoBean(String quiz, String answer) {
+	final private String quiz;		// 問題
+	final private String answer;	// 回答
+	
+	QuizInfoBean(final String quiz, final String answer) {
 		this.quiz = quiz;
 		this.answer = answer;
 	}
-
+	
+	public static ArrayList<QuizInfoBean> quizList(final ResultSet rs) throws SQLException {
+		ArrayList<QuizInfoBean> quizList = new ArrayList<>();
+		
+		try {
+			while(rs.next()) {
+				QuizInfoBean quizInfoObj = new QuizInfoBean(rs.getString("quiz"), rs.getString("answer"));
+				// QuizInfoBeanオブジェクトをクイズリスト(quizList)の要素へ追加
+				quizList.add(quizInfoObj);
+			}
+		} catch (SQLException e) {
+			System.out.println("SQLException : " + e.getMessage());
+		} finally {
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				System.out.println("SQLException : " + e.getMessage());
+			}
+		}
+		
+		return quizList;
+	}
+	
 	public String quiz() {
 		return quiz;
 	}
