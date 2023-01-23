fa9572162bb691b20d4c85d48ab4ee4dd58c1665
Rqixy <yuukisato1013@gmail.com>
2023-01-23 17:32:22 +0900

    正答率の求める式を修正、Goalオブジェクトを取得して各処理を行うように変更、コンストラクタでユニセフのURLを受け取るように変更

--- src/model/Result.java
+++ src/model/Result.java
@@ -3,7 +3,8 @@ package model;
 import java.sql.SQLException;
 import java.util.HashMap;
 
-import db.ClearStatusQuery;
+import bean.GoalBean;
+import db.ClearStatusDao;
 
 /**
  * 問題が終了した時の処理をまとめたクラス
@@ -15,14 +16,16 @@ public class Result {
 	private final float clearRate;
 	private final int allClear;
 	private final int clear;
+	private static String unicefUrl;
 	
-	private Result(final int clearStatusInDb, final float correctAnswerRate) {
+	private Result(final int clearStatusInDb, final float correctAnswerRate, final String unicefUrl) {
 		this.clearStatusInDb = clearStatusInDb;
 		this.correctAnswerRate = correctAnswerRate;
 		this.allClearRate = 1f;
 		this.clearRate = 0.6f;
 		this.allClear = 2;
 		this.clear = 1;
+		Result.unicefUrl = unicefUrl;
 	}
 	
 	/**
@@ -30,40 +33,48 @@ public class Result {
 	 * @param sessionInfo
 	 * @throws SQLException
 	 */
-	public static void clear(LoginUserBean loginUserBean, Quiz quiz) throws SQLException {
+	public static void clear(LoginUserBean loginUser, Quiz quiz) throws SQLException {
 		try {
 			// ユーザーIDと目標番号の取得
-			int userId = loginUserBean.getId();
 			int goalNumber = quiz.goalNumber();
-			float answerCount = quiz.answerCount();
-			float maxQuizCount = quiz.maxQuizCount();
 			
 			// クリア状況の更新処理
 			// クリア状況DBの処理するクラスの初期化
-			ClearStatusQuery clearStatusQuery = new ClearStatusQuery();
-			// DB内のクリア状況と更新するカラム名を取得
-			int clearStatusInDb = clearStatusQuery.selectOne(userId, goalNumber);
+			ClearStatusDao clearStatusDao = new ClearStatusDao();
+			// DB内のクリア状況を取得
+			GoalBean goal = clearStatusDao.goal(loginUser, quiz);
+			int clearStatusInDb = goal.clearStatus();
 			// 正答率を求める
+			float answerCount = (float)quiz.answerCount();
+			float maxQuizCount = (float)quiz.maxQuizCount();
 			float correctAnswerRate = answerCount / maxQuizCount;
 			
-			Result result = new Result(clearStatusInDb, correctAnswerRate);
+			String unicefUrl = unicefUrlByGoalNumber(goalNumber);
+			Result result = new Result(clearStatusInDb, correctAnswerRate, unicefUrl);
 			
 			// 正答数を確認して、正答数に応じたクリア状況を更新
 			// 1度も全問正解したことがなく、初めて全問正解なら数字を2に更新する
 			if (result.isAllClear()) {
-				clearStatusQuery.update(goalNumber, result.allClear, userId);
+				clearStatusDao.update(loginUser, quiz, result.allClear);
 			}
 			// 1度もクリアしたことがなく(数字が0)で、初めて正答率が6割超えたら1に更新する
 			if (result.isClear()) {
-				clearStatusQuery.update(goalNumber, result.clear, userId);
+				clearStatusDao.update(loginUser, quiz, result.clear);
 			}
 		} catch (SQLException e) {
 			System.out.println("SQLException : " + e.getMessage());
 		}
 	}
 	
-	// 目標番号からユニセフのURLを取得する処理
-	public static String getUnicefUrl(Quiz quiz) {
+	public static String unicefUrl() {
+		return unicefUrl;
+	}
+	
+	/**
+	 * 目標番号からユニセフのURLを取得する処理
+	 * @param goalNumber
+	 */
+	private static String unicefUrlByGoalNumber(int goalNumber) {
 		// ハッシュ配列で目標番号とSDGsの17の目標のユニセフサイトのURLを格納
 		HashMap<Integer, String> goals = new HashMap<Integer, String>();
 
@@ -86,13 +97,19 @@ public class Result {
         goals.put(16, "16-peace");
         goals.put(17, "17-partnerships");
         
-        return  "https://www.unicef.or.jp/kodomo/sdgs/17goals/" + goals.get(quiz.goalNumber());
+        return "https://www.unicef.or.jp/kodomo/sdgs/17goals/" + goals.get(goalNumber);
 	}
 	
+	/**
+	 * 全問正解判定
+	 */
 	private boolean isAllClear() {
 		return clearStatusInDb < allClear && correctAnswerRate == allClearRate;
 	}
 	
+	/**
+	 * 正解判定
+	 */
 	private boolean isClear() {
 		int notClear = 0;
 		return clearStatusInDb == notClear && correctAnswerRate >= clearRate;
