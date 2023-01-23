a25f873e483a10821b86609a3f6b0a01583ff48c
Rqixy <yuukisato1013@gmail.com>
2023-01-23 12:06:05 +0900

    ユーザーIDをクラスから受け取るように変更、非ログイン時の処理変更

--- src/servlet/HomeServlet.java
+++ src/servlet/HomeServlet.java
@@ -4,8 +4,6 @@ import java.io.IOException;
 import java.sql.SQLException;
 import java.util.HashMap;
 
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -14,6 +12,9 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import db.ClearStatusQuery;
+import model.Login;
+import model.LoginUserBean;
+import model.ScreenTransition;
 
 /**
  * ホーム画面を表示する際にログインしたユーザーのクリア状況を確認し準備する処理
@@ -25,33 +26,31 @@ public class HomeServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		try {
 			request.setCharacterEncoding("UTF-8");
+			
+			// ログインしてなかったら、ログイン画面へリダイレクト
+			if (!Login.loggedInUser(request)) {
+				ScreenTransition.redirectLogin(request, response);
+				return;
+			}
 			// セッションスコープの準備
 			HttpSession session = request.getSession();
 			
-			// ログインしてなかったら、ログイン画面へリダイレクト
-			if (session.getAttribute("userId") == null) {
-				response.sendRedirect(request.getContextPath() + "/LoginServlet");
-				return;
-			}
-			
-			int userId = (int)session.getAttribute("userId");
+			LoginUserBean loginUserBean = (LoginUserBean)session.getAttribute("user");
 			// クリア状況DBの処理するクラスの初期化
 			ClearStatusQuery clearStatusQuery = new ClearStatusQuery();
 			// 送られてきたユーザーIDのクリア状況のデータが作成されているか確認し、クリア状況のデータが作成されていなかったら、新規に作成
-			if (!clearStatusQuery.exist(userId)) {
-				clearStatusQuery.insert(userId);
+			if (!clearStatusQuery.exist(loginUserBean.getId())) {
+				clearStatusQuery.insert(loginUserBean.getId());
 			}
 
 			// ユーザーIDからクリア状況のテーブルを参照し、参照したクリア状況を配列に格納
-			HashMap<Integer, Integer> clearStatus = clearStatusQuery.select(userId);
+			HashMap<Integer, Integer> clearStatus = clearStatusQuery.select(loginUserBean.getId());
 
 			// クリア状況の内容をセッションに保存
 			session.setAttribute("clearStatus", clearStatus);
 
 			// Top画面へ表示
-			ServletContext s = request.getServletContext();
-			RequestDispatcher rd = s.getRequestDispatcher("/WEB-INF/view/home.jsp");
-			rd.forward(request, response);
+			ScreenTransition.forward(request, response, "home.jsp");
 		} catch (ServletException e) {
 			System.out.println("ServletException : " + e.getMessage());
 		} catch (SQLException e) {
