eba94c194f88027ffcff91104318ebff8fee612c
Rqixy <yuukisato1013@gmail.com>
2023-01-23 11:02:05 +0900

    answerListを取得する処理追加し、フィールドを定数化、コンストラクタをprivateに変更

--- src/model/AnswersBean.java
+++ src/model/AnswersBean.java
@@ -1,18 +1,50 @@
 package model;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 /*
  * 回答一覧管理クラス
  */
 public class AnswersBean {
-	private String correct;		// 正解
-	private String incorrect1;	// 誤答1
-	private String incorrect2;	// 誤答2
-	private String incorrect3;	// 誤答3
+	private final String correct;		// 正解
+	private final String incorrect1;	// 誤答1
+	private final String incorrect2;	// 誤答2
+	private final String incorrect3;	// 誤答3
 
-	public AnswersBean(String correct, String incorrect1, String incorrect2, String incorrect3) {
+	AnswersBean(final String correct, final String incorrect1, final String incorrect2, final String incorrect3) {
 		this.correct = correct;
 		this.incorrect1 = incorrect1;
 		this.incorrect2 = incorrect2;
 		this.incorrect3 = incorrect3;
+	}
+	
+	public static ArrayList<AnswersBean> answersList(final ResultSet rs) throws SQLException {
+		ArrayList<AnswersBean> answerList = new ArrayList<>();
+		
+		try {
+			// 回答情報をオブジェクトに格納する
+			while(rs.next()) {
+				AnswersBean answersObj = new AnswersBean(
+						rs.getString("correct"), 
+						rs.getString("incorrect_1"), 
+						rs.getString("incorrect_2"),	
+						rs.getString("incorrect_3"));
+				// AnswersBeanオブジェクトを回答リスト(answerList)の要素へ追加
+				answerList.add(answersObj);
+			}
+		} catch (SQLException e) {
+			System.out.println("SQLException : " + e.getMessage());
+		} finally {
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				System.out.println("SQLException : " + e.getMessage());
+			}
+		}
+		
+		return answerList;
 	}
 
 	public String correct() {
