c633a9d7ce1c051d907270bfe865c7155a34472d
Rqixy <yuukisato1013@gmail.com>
2023-01-26 14:22:37 +0900

    定数化

--- src/bean/QuizInfoBean.java
+++ src/bean/QuizInfoBean.java
@@ -4,8 +4,8 @@ package bean;
  * クイズ情報管理クラス
  */
 public class QuizInfoBean {
-	final private String quiz;		// 問題
-	final private String answer;	// 回答
+	private final String quiz;		// 問題
+	private final String answer;	// 回答
 	
 	public QuizInfoBean(final String quiz, final String answer) {
 		this.quiz = quiz;
