023bd969b959b895e58d9cd6b7f60fb29eb722ca
Rqixy <yuukisato1013@gmail.com>
2023-01-26 14:17:45 +0900

    リダイレクト処理をクラスにわけた

---
+++ src/transition/Redirect.java
@@ -0,0 +1,41 @@
+package transition;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Redirect {
+	private Redirect() {}
+	
+	/**
+	 * ログインへリダイレクトする処理
+	 * @param request
+	 * @param response
+	 * @throws IOException
+	 */
+	public static void login(final HttpServletRequest request, final HttpServletResponse response) throws IOException {
+		try {
+			// セッション破棄
+			request.getSession().invalidate();
+			// リダイレクト
+			response.sendRedirect(request.getContextPath() + "/login");
+		} catch (IOException e) {
+			System.out.println("IOException : " + e.getMessage());
+		}
+	}
+	
+	/**
+	 * ホームへリダイレクトする処理
+	 * @param request
+	 * @param response
+	 * @throws IOException
+	 */
+	public static void home(final HttpServletRequest request, final HttpServletResponse response) throws IOException {
+		try {
+			response.sendRedirect(request.getContextPath() + "/home");
+		} catch (IOException e) {
+			System.out.println("IOException : " + e.getMessage());
+		}
+	}
+}
--- src/transition/ScreenTransition.java
+++ src/transition/ScreenTransition.java
@@ -9,9 +9,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * 画面遷移やリダイレクト処理をまとめたクラス
+ * 画面遷移処理
  */
 public class ScreenTransition {
+	private ScreenTransition() {}
+	
 	/**
 	 * jspファイルへフォワードする処理
 	 * @param request
@@ -21,45 +23,12 @@ public class ScreenTransition {
 	 * @throws ServletException
 	 */
 	public static void forward(final HttpServletRequest request, final HttpServletResponse response, final String jsp) throws IOException, ServletException {
-		try {
-			if (!jsp.endsWith(".jsp")) {
-				throw new IOException("jspファイルを指定してください");
-			}
-			ServletContext s = request.getServletContext();
-			RequestDispatcher rd = s.getRequestDispatcher("/WEB-INF/view/" + jsp);
-			rd.forward(request, response);
-		} catch (ServletException e) {
-			System.out.println("ServletException : " + e.getMessage());
-		} catch (IOException e) {
-			System.out.println("IOException : " + e.getMessage());
+		if (!jsp.endsWith(".jsp")) {
+			throw new IOException("jspファイルを指定してください");
 		}
-	}
-	
-	/**
-	 * ログインへリダイレクトする処理
-	 * @param request
-	 * @param response
-	 * @throws IOException
-	 */
-	public static void redirectLogin(final HttpServletRequest request, final HttpServletResponse response) throws IOException {
-		try {
-			response.sendRedirect(request.getContextPath() + "/login");
-		} catch (IOException e) {
-			System.out.println("IOException : " + e.getMessage());
-		}
-	}
-	
-	/**
-	 * ホームへリダイレクトする処理
-	 * @param request
-	 * @param response
-	 * @throws IOException
-	 */
-	public static void redirectHome(final HttpServletRequest request, final HttpServletResponse response) throws IOException {
-		try {
-			response.sendRedirect(request.getContextPath() + "/home");
-		} catch (IOException e) {
-			System.out.println("IOException : " + e.getMessage());
-		}
+		
+		ServletContext s = request.getServletContext();
+		RequestDispatcher rd = s.getRequestDispatcher("/WEB-INF/view/" + jsp);
+		rd.forward(request, response);
 	}
 }
