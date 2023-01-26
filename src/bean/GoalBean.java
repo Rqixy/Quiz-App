9b6d0453454ff9f39b3708fa62370df4842c52c9
Rqixy <yuukisato1013@gmail.com>
2023-01-26 14:22:15 +0900

    ユーザーIDとカラム名削除

--- src/bean/GoalBean.java
+++ src/bean/GoalBean.java
@@ -4,30 +4,16 @@ package bean;
  * 1つの目標のデータを保存するクラス
  */
 public class GoalBean {
-	private final int userId;
-	private final String columnName;
 	private final int goalNumber;
-	private int clearStatus;
+	private final int clearStatus;
 	
 	/**
-	 * @param userId
-	 * @param columnName
 	 * @param goalNumber
 	 * @param clearStatus
 	 */
-	public GoalBean(final int userId, final String columnName, final int goalNumber, final int clearStatus) {
-		this.userId = userId;
-		this.columnName = columnName;
+	public GoalBean(final int goalNumber, final int clearStatus) {
 		this.goalNumber = goalNumber;
 		this.clearStatus = clearStatus;
-	}
-	
-	public int userId() {
-		return userId;
-	}
-
-	public String columnName() {
-		return columnName;
 	}
 	
 	public int goalNumber() {
