4454bc3388aecb7e8729f67a56e1a57985caa25b
Rqixy <yuukisato1013@gmail.com>
2023-01-26 21:39:38 +0900

    コンストラクタをprivateに、staticメソッドに変更

--- src/db/QuizInfoDao.java
+++ src/db/QuizInfoDao.java
@@ -9,13 +9,15 @@ import bean.QuizInfoBean;
  * DB内の問題一覧を取得するクラス
  */
 public class QuizInfoDao extends Db {
+	private QuizInfoDao() {}
+	
 	/**
 	 * 渡された目標番号の問題一覧を取得
 	 * @param goalNumber	目標番号
 	 * @return quizList		クイズ情報
 	 * @throws SQLException
 	 */
-	public ArrayList<QuizInfoBean> quizInfoList(final int goalNumber) throws SQLException {
+	public static ArrayList<QuizInfoBean> quizInfoList(final int goalNumber) throws SQLException {
 		dbInit();
 		ArrayList<QuizInfoBean> quizInfoList = new ArrayList<>();
 		
@@ -23,17 +25,12 @@ public class QuizInfoDao extends Db {
 			rs = executeSelect("SELECT quiz, answer FROM quiz_info WHERE goal_number = ?", goalNumber);
 			while(rs.next()) {
 				QuizInfoBean quizInfoObj = new QuizInfoBean(rs.getString("quiz"), rs.getString("answer"));
-				// QuizInfoBeanオブジェクトをクイズリスト(quizList)の要素へ追加
 				quizInfoList.add(quizInfoObj);
 			}
 		} catch (SQLException e) {
 			System.out.println("SQLException : " + e.getMessage());
 		} finally {
-			try {
-				dbClose();
-			} catch (SQLException e) {
-				System.out.println("SQLException : " + e.getMessage());
-			}
+			dbClose();
 		}
 		
 		return quizInfoList;
