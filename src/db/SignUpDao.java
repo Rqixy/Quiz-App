9645f86f5ecd1faa41135720f9122571a5f0f616
Rqixy <yuukisato1013@gmail.com>
2023-02-02 16:10:26 +0900

    登録されているユーザー全て取得

--- src/db/SignUpDao.java
+++ src/db/SignUpDao.java
@@ -1,14 +1,34 @@
 package db;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 
-import bean.RegisterUserBean;
+import bean.RegistUserBean;
 
 public class SignUpDao extends Db {
 	private SignUpDao() {}
 	
+	public static ArrayList<String> selectAll() throws SQLException {
+		dbInit();
+		ArrayList<String> registeredNameList = new ArrayList<String>();
+		
+		try {
+			//SQLを実行
+			rs = executeSelect("SELECT * FROM users");
+			while(rs.next()) {
+				registeredNameList.add(rs.getString("name"));
+			}
+		} catch (SQLException e) {
+			System.out.println("SQLException : " + e.getMessage());
+		} finally {
+			dbClose();
+		}
+		
+		return registeredNameList;
+	}
+	
 	//登録処理
-	public static boolean userAdd(RegisterUserBean registUser) throws SQLException {
+	public static boolean userAdd(RegistUserBean registUser) throws SQLException {
 		//初期化
 		dbInit();
 		boolean flag = false;
