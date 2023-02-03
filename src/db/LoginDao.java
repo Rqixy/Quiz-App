7c0fc76c9dea73397597cc5ccdb113ce221d66bb
Rqixy <yuukisato1013@gmail.com>
2023-02-03 09:46:25 +0900

    パスワードのハッシュ化処理LoginServletに移動

--- src/db/LoginDao.java
+++ src/db/LoginDao.java
@@ -1,8 +1,5 @@
 package db;
 
-import java.math.BigInteger;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.sql.SQLException;
 
 import bean.LoginUserBean;
@@ -10,24 +7,18 @@ import bean.LoginUserBean;
 public class LoginDao extends Db {
 	private LoginDao() {}
 	
-	public static LoginUserBean selectUser(String name, String pass) throws SQLException, NoSuchAlgorithmException {
+	public static LoginUserBean selectUser(String name, String pass) throws SQLException {
 		dbInit();
 		LoginUserBean loginUser = null;
 		
 		try {
-			MessageDigest md = MessageDigest.getInstance("SHA3-256");
-			byte[] bytePassword = md.digest(pass.getBytes());
-			String encodedPass = String.format("%040x", new BigInteger(1, bytePassword));
-			
 			//SQLを実行
-			rs = executeSelect("select id, name from users where name = ? and pass = ?", name, encodedPass);
+			rs = executeSelect("select id, name from users where name = ? and pass = ?", name, pass);
 			
 			//実行結果を貰う
 			if(rs.next()) {
 				loginUser = new LoginUserBean(rs.getInt("id"), rs.getString("name"));
 			}
-		} catch (NoSuchAlgorithmException e) {
-			e.printStackTrace();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} finally {
